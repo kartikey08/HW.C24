74cb07ddae99425459bf1258d15138b66efdefcd
kartikey08 <71174170+kartikey08@users.noreply.github.com>
2020-10-17 14:50:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# HW.C24
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ rect.js
@@ -0,0 +1,21 @@
+class RectG {
+    constructor(x, y, width, height){
+        var options={
+            isStatic: true
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+    }
+    display(){
+        var posR= this.body.position;
+        rectMode(CENTER);
+        fill("brown");
+        strokeWeight(0);
+        rect(posR.x, posR.y, this.width, this.height);
+        //this.body.setCollider("rectangle", 0,0, 800, 30);
+    }
+}
